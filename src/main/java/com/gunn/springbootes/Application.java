c7fae1e1b1eaddae8c4e4b3a6714932364e18ffa
ganjunhui <317729530@qq.com>
2020-02-13 13:15:24 +0800

    测试main的注释说明

--- src/main/java/com/gunn/springbootes/Application.java
+++ src/main/java/com/gunn/springbootes/Application.java
@@ -29,14 +29,20 @@ public class Application {
 //		properties.add(Property.builder().name("name").type("text").build());
 //		indexOperation.createMapping("test", "testType", properties);
 
+		// 删除已有的索引
 		indexOperation.deleteIndex("field");
+		// 创建索引和映射
 		fieldQuery.createIndexAndMapping();
+		// 随便插入一条场地数据
 		fieldQuery.index();
+		// 随便插入几条日期的子文档
 		fieldDateQuery.index();
 
+		// 测试根据文档id查询功能
 		Field byId = fieldQuery.getById("1234_10");
 		// 执行查询时，文档并未完全插入，所以线程休眠一段时间
 		TimeUnit.SECONDS.sleep(2);
+		// 测试根据查询日期子文档定位到场地副文档功能
 		Field field = fieldQuery.searchByFeildDates("2020-01-13");
 		System.out.println("end");
 	}
